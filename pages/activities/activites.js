ee062cbc2dca49a262aa9f02b53587106a6f7d0f
Ross Gray <rosswgray@gmail.com>
2020-11-25 15:05:40 +0800

    trying to connect back and front

---
+++ pages/activities/activites.js
@@ -0,0 +1,115 @@
+// pages/activities/activites.js
+
+Page({
+  /**
+   * Page initial data
+   */
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+
+    imgUrls: [
+      'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
+      'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
+      'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
+    ],
+    indicatorDots: false,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
+  },
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
+  },
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
+  },
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
+  },
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
+  },
+
+  goToShow: function(event) {
+    const id = event.currentTarget.dataset.id
+    console.log(id)
+    navigateTo({
+      url: `pages/activity/activity?id=${id}`,
+    })
+  },
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    const page = this;
+
+    wx.request({
+      url: 'https://aimy-teaser.herokuapp.com/api/v1/activities',
+      success: function(res) {
+        const activities = res.data
+        page.setData(activities)
+        console.log(activities)
+      }
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})
